26f81504fcd39a31ac7e36ca4c998e1cfd6afb30
OlgaMustaeva <olgaguzina@mail.ru>
2023-11-07 22:04:28 +0500

    fixed

--- src/ru/Java/ITMO/basics/lesson4/Array.java
+++ src/ru/Java/ITMO/basics/lesson4/Array.java
@@ -1,20 +1,18 @@
 package ru.Java.ITMO.basics.lesson4;
 
 public class Array {
-
-    public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5};
-        boolean containsOneOrThree = false;
-        for (int number : array) {
-            if (number == 1 || number == 3) {
-                containsOneOrThree = true;
-                break;
+    public static boolean containsOneOrThree(int[] arr) {
+        for (int num : arr) {
+            if (num == 1 || num == 3) {
+                return true;
             }
         }
-        if (containsOneOrThree) {
-            System.out.println("Массив содержит число 1 или 3");
-        } else {
-            System.out.println("Массив не содержит число 1 или 3");
-        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[] arr1 = {2, 4, 6, 8};
+        System.out.println(containsOneOrThree(arr1));
+        int[] arr2 = {1, 5, 9, 3};
+        System.out.println(containsOneOrThree(arr2));
     }
 }
