fc3d415909bbacaa0176602e2d44368fdaf2c5c9
OlgaMustaeva <olgaguzina@mail.ru>
2023-11-09 22:17:29 +0500

    fixed

--- src/ru/Java/ITMO/basics/lesson5/Byaka.java
+++ src/ru/Java/ITMO/basics/lesson5/Byaka.java
@@ -1,15 +1,15 @@
 package ru.Java.ITMO.basics.lesson5;
 public class Byaka {
-    public static String censorText(String text) {
-        return text.replaceAll("бяка", "[вырезано цензурой]");
+    public static String censorText(String text, String censorWord, String replacement) {
+        return text.replaceAll(censorWord, replacement);
     }
     public static void main(String[] args) {
         String originalText = "В тексте есть слово бяка несколько раз";
-        String censoredText = censorText(originalText);
-
+        String censorWord = "бяка";
+        String replacement = "[вырезано цензурой]";
+        String censoredText = censorText(originalText, censorWord, replacement);
         System.out.println("Оригинальный текст:");
         System.out.println(originalText);
-
         System.out.println("\nТекст после цензуры:");
         System.out.println(censoredText);
     }
